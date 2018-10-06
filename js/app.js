63d9fa7b12d0664a815a93f0119bc9b59ce46c67
RonWaller <ron.waller7@gmail.com>
2018-10-06 19:19:56 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let request = new XMLHttpRequest();
+let url = 'http://quotes.rest/qod.json';
+
+request.onload = function() {
+    if (this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        // console.log(response);
+        getElements(response);
+    }
+}
+
+request.open("GET", url, true);
+request.send();
+
+getElements = function(response) {
+    let author = response.contents.quotes[0].author;
+    let quote = response.contents.quotes[0].quote;
+    let background = response.contents.quotes[0].background;
+    let link = response.contents.quotes[0].permalink;
+    let title = response.contents.quotes[0].title;
+    // let styleElem = document.head.appendChild(document.createElement("style"));
+    // console.log(author);
+
+
+
+
+    document.querySelector('.heading').innerHTML = title;
+    document.querySelector('.quote').innerHTML = '"' + quote + '"';
+    document.querySelector('.author').innerHTML = '-- ' + author;
+    document.querySelector('.cite').setAttribute('cite', link);
+    // styleElem.innerHTML = ".wrapper::before {background-image: url(" + background + ");}";
+    document.querySelector('.wrapper').style.backgroundImage = 'url(' + background + ')';
+
+
+}
